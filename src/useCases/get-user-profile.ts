5238e5cd918759817c397e5e3af01209c4aa06f0
Andre Grillo <andrell.grillo@gmail.com>
2023-05-30 10:19:14 -0300

    Obter Perfil de Usuario Logado

---
+++ src/useCases/get-user-profile.ts
@@ -0,0 +1,30 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetuserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetuserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetuserProfileUseCaseRequest): Promise<GetuserProfileUseCaseResponse> {
+    //
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
