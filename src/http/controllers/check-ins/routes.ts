35da390e153d7b3aebbdc077f309eda075051f18
Andre Grillo <andrell.grillo@gmail.com>
2023-06-01 13:06:02 -0300

    Controlleres and Routes - GYMS

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+}
---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,24 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+  const { latitude, longitude } = nearbyGymBodySchema.parse(request.body)
+
+  const fetchNearbyGymsuseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsuseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({ gyms })
+}
